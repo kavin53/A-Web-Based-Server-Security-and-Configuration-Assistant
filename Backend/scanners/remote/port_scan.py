79be181037a37b2bf34f15e5245ac2e05488556d
kavin53 <kavin53>
2026-01-05 15:45:54 +0530

    create portScan method

---
+++ Backend/scanners/remote/port_scan.py
@@ -0,0 +1,40 @@
+import socket
+from unittest import result
+
+COMMON_PORTS = {
+    21:"FTP",
+    22:"SSH",
+    23:"TELNET",
+    25:"SMTP",
+    53:"DNS",
+    80:"HTTP",
+    110:"POP3",
+    143:"IMAP",
+    443:"HTTPS",
+    3306:"MYSQL",
+    8080:"HTTP-ALT"
+}
+
+def scan_ports(target, timeout=1):
+    results = []
+
+    for port , service in COMMON_PORTS.items():
+        try:
+            sock =socket.socket(socket.AF_INT, socket.SOCK_STREAM)
+            sock.settimeout(timeout)
+            result = sock.connect_ex((target,port))
+            sock.close()
+
+            if result == 0:
+                risk ="high" if port in[21,23,3306] else "meadium"
+                results.append({
+                    "port":port,
+                    "service":service,
+                    "status":"open",
+                    "risk":risk,
+                    "recommendation":f"Restrict access to {service} if not required"
+                })
+
+        except Exception:
+            continue
+    return result
