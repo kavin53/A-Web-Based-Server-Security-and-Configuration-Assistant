903164efc44644723b8b73420576ba1351e57334
kavin53 <jayarathnekavindu510@gmail.com>
2026-01-06 17:13:09 +0530

    updated

--- Backend/app.py
+++ Backend/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from scanners.remote.port_scan import scan_ports
+from scanners.remote.headers_scan import scan_headers
+
 
 app = Flask(__name__)
 CORS(app)
@@ -9,25 +11,29 @@ CORS(app)
 def health():
     return jsonify({"status": "running"}), 200
 
-@app.route("/run-scan", methods=['POST'])
+@app.route("/run-scan", methods=["POST"])
 def run_scan():
     data = request.json or {}
     scan_type = data.get("type")
-
-    if scan_type != "remote":
-        return jsonify({"error": "Only remote scan implemented"}), 400
-    
     target = data.get("target")
+
     if not target:
         return jsonify({"error": "Target is required"}), 400
-    
-    results = scan_ports(target)
+
+    if scan_type == "remote":
+        results = scan_ports(target)
+
+    elif scan_type == "headers":
+        results = scan_headers(target)
+
+    else:
+        return jsonify({"error": "Invalid scan type"}), 400
 
     return jsonify({
         "target": target,
-        "scan_type": "remote",
+        "scan_type": scan_type,
         "results": results
-    }),200
+    }), 200
 
 if __name__ == "__main__":
     app.run(debug=True)
