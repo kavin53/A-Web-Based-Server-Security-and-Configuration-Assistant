bf58f2a643b5719ea424f012d8a345b37cb536d7
kavin53 <jayarathnekavindu510@gmail.com>
2026-01-06 16:46:23 +0530

    re edit

--- Backend/scanners/remote/port_scan.py
+++ Backend/scanners/remote/port_scan.py
@@ -1,39 +1,39 @@
 import socket
-from unittest import result
 
 COMMON_PORTS = {
-    21:"FTP",
-    22:"SSH",
-    23:"TELNET",
-    25:"SMTP",
-    53:"DNS",
-    80:"HTTP",
-    110:"POP3",
-    143:"IMAP",
-    443:"HTTPS",
-    3306:"MYSQL",
-    8080:"HTTP-ALT"
+    21: "FTP",
+    22: "SSH",
+    23: "TELNET",
+    25: "SMTP",
+    53: "DNS",
+    80: "HTTP",
+    110: "POP3",
+    143: "IMAP",
+    443: "HTTPS",
+    3306: "MySQL",
+    8080: "HTTP-ALT",
+    8000: "HTTP-DEV"
 }
 
 def scan_ports(target):
     results = []
 
-    for port , service in COMMON_PORTS.items():
+    for port, service in COMMON_PORTS.items():
         try:
-            sock =socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            sock.settimeout(1)
-            
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.settimeout(2)
 
-            if sock.connec_ex((target,port))==0:
+            if sock.connect_ex((target, port)) == 0:
                 results.append({
-                    "port":port,
-                    "service":service,
-                    "status":"open",
-                    "risk":"meadium" if port not in [80,443] else "low",
-                    "recommendation":f"Restrict access to {service} if not required"
+                    "port": port,
+                    "service": service,
+                    "status": "open",
+                    "risk": "low" if port in [80, 443] else "medium",
+                    "recommendation": f"Restrict access to {service} if not required"
                 })
-                sock.close()
 
-        except Exception as e:
+            sock.close()
+        except Exception:
             continue
+
     return results
