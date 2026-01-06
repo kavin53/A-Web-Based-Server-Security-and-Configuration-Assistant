03848667082d12ea92812c52ad71b73b8d9a7bcd
kavin53 <jayarathnekavindu510@gmail.com>
2026-01-06 17:13:18 +0530

    add headers

---
+++ Backend/scanners/remote/headers_scan.py
@@ -0,0 +1,39 @@
+import requests
+
+SECURITY_HEADERS = {
+    "Content-Security-Policy": "Protects against XSS attacks",
+    "X-Frame-Options": "Prevents clickjacking",
+    "X-Content-Type-Options":"prevents MIME sniffing",
+    "Strict-Transport-Security": "Enforces HTTPS",
+    "Referrer-Policy":"Controls referrer information",
+    "Permissions-Policy":"Restrict browser features"
+}
+
+def scan_headers(target):
+    results = []
+
+    if not target.startswith("http"):
+        target = "http://" +target
+
+    try:
+        response = requests.get(target, timeout=5)
+        headers = response.headers
+
+        for header,purpose in SECURITY_HEADERS.items():
+            if header not in headers:  
+                results.append({
+                        "header": header,
+                        "status": "missing",
+                        "risk": "medium",
+                        "description": purpose,
+                        "recommendation": f"Add {header} to HTTP response"
+                    })
+
+        return results
+            
+    except Exception as e:
+        return[{
+            "error": str(e),
+            "risk": "high",
+            "recommendation": "Target unreachable or invalid"
+        }]
